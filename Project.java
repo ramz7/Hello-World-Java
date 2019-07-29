fae789e27b9289548eabbb84640aa7e05a4e4d87
ramz7 <39064079+ramz7@users.noreply.github.com>
2019-07-29 17:37:05 +0530

    Update Project.java

--- Project.java
+++ Project.java
@@ -1,5 +1,5 @@
 public class Project{
-    public static void main(String[] args) {
+    public static void man(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello World");
     }
